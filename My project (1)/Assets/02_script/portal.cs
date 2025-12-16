abd28aeabc8b60ab9409fe1822c5a132c5810aa4
luca5011 <qqluca1234@gmail.com>
2025-12-16 18:39:25 +0900

    [추가] 스테이지 2,3제작

--- My project (1)/Assets/02_script/portal.cs
+++ My project (1)/Assets/02_script/portal.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class portal : MonoBehaviour
 {
+    public Slider HP_slider;
     public string Nextscene;
+    [Header("스테이지가 양수면 체크")]
+    public bool flag = true;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "player")
         {
-            Application.LoadLevel(Nextscene);
+            if (!flag || HP_slider.value>0)
+            {
+                Application.LoadLevel(Nextscene);
+            }
         }
 
 
